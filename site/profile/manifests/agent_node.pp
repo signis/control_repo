1628e4b0eded06164e7e45d65715657937461ffe
Signis Vavere <signis.vavere@gmail.com>
2022-02-17 22:57:23 +0200

    Update agent_node.pp

--- site/profile/manifests/agent_node.pp
+++ site/profile/manifests/agent_node.pp
@@ -2,11 +2,11 @@ class profile::agent_node {
   include dockeragent
   dockeragent::node { 'web.puppet.vm': }
   dockeragent::node { 'db.puppet.vm': }
-  host {'web.pupper.vm':
+  host {'web.puppet.vm':
     ensure => present,
     ip => '172.18.0.2',
   }
-  host {'db.pupper.vm':
+  host {'db.puppet.vm':
     ensure => present,
     ip => '172.18.0.3',
   }    
